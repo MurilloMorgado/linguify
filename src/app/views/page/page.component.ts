082b779998aaad105ce7e624e9743426b447513f
Murillo Morgado <mumorgadok@hotmail.com>
2024-06-18 21:02:40 -0300

    estilizando introduçao

--- src/app/views/page/page.component.ts
+++ src/app/views/page/page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { IntroducaoComponent } from './introducao/introducao.component';
 
 @Component({
   selector: 'app-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, IntroducaoComponent],
   templateUrl: './page.component.html',
   styleUrl: './page.component.css'
 })
