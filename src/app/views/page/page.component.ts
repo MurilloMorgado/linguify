4bf15868702e02f4cf6bfc62a958c39951ddc981
Murillo Morgado <mumorgadok@hotmail.com>
2024-06-27 19:53:17 -0300

    criando o footer

--- src/app/views/page/page.component.ts
+++ src/app/views/page/page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { IntroducaoComponent } from './introducao/introducao.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-page',
   standalone: true,
-  imports: [HeaderComponent, IntroducaoComponent],
+  imports: [HeaderComponent, IntroducaoComponent, FooterComponent],
   templateUrl: './page.component.html',
   styleUrl: './page.component.css'
 })
